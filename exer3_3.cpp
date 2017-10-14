d6ba8299a8db0688dd041693a0112faac28829ad
nicolanded <31621836+nicolanded@users.noreply.github.com>
2017-10-14 14:51:43 +0800

    homework

---
+++ exer3_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter a:";
+	cin>>a;
+	if(a%3==0)
+	  cout<<"YES"<<endl;
+	else
+	  if(a%7==0)
+	  cout<<"YES"<<endl;
+	else
+	  cout<<"NO"<<endl;
+}
+ 
